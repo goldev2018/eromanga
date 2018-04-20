f8f4b673689876b48aec65a2fb184645c1fa7a87
islasDC <slaaash.dc@gmail.com>
2018-04-20 09:46:54 +0800

    Initial commit

---
+++ index.php
@@ -0,0 +1,193 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<title>Ero Manga Hotel</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="HALO photography portfolio template">
+	<meta name="keywords" content="photography, portfolio, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->   
+	<link href="img/favicon.ico" rel="shortcut icon"/>
+
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i" rel="stylesheet">
+
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/animate.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+	<link rel="stylesheet" href="css/slash.css"/>
+
+
+	<!--[if lt IE 9]>
+	  <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	  <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+	<div id="navbar">
+  <font color="white" size="78px"><span><h1><b>EROMANGA</b> HOTEL</h1></span></font>
+</div>
+	<!-- Header section start -->
+	<header class="header-section sp-pad">
+		<h3 class="site-logo">EROMANGA</h3>
+		<form class="search-top">
+			<button class="se-btn"><i class="fa fa-search"></i></button>
+			<input type="text" placeholder="Search.....">
+		</form>
+		<div class="nav-switch">
+			<i class="fa fa-bars"></i>
+		</div>
+		<nav class="main-menu">
+			<ul>
+				<li><a href="index.html">Home</a></li>
+				<li><a href="about.html">about us</a></li>
+				<li><a href="#">Services</a></li>
+				<li><a href="portfolio.html">Portfolio</a></li>
+				<li><a href="blog.html">Blog</a></li>
+				<li><a href="contact.html">Contact</a></li>
+			</ul>
+		</nav>
+	</header>
+	<!-- Header section end -->
+
+
+	<!-- Hero section start -->
+	<section class="hero-section">
+		<div class="hero-slider owl-carousel">
+			<div class="hs-item set-bg sp-pad" data-setbg="img/hero-slider/Header_img.png">
+				<div class="hs-text">
+					<!-- <br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br> -->
+					<h2 class="hs-des">EROMANGA HOTEL</h2>
+					<!-- <p class="hs-des">We Create Awesome <br>Photographies and more</p> -->
+				</div>
+			</div>
+			<!-- <div class="hs-item set-bg sp-pad" data-setbg="img/hero-slider/2.jpg">
+				<div class="hs-text">
+					<h2 class="hs-title">Halo</h2>
+					<p class="hs-des">We Create Awesome <br>Photographies and more</p>
+				</div>
+			</div> -->
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+
+	<!-- Intro section start -->
+	<section class="intro-section sp-pad spad first-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-4 intro-text">
+					<!-- <span class="sp-sub-title">Amazing studio</span>
+					<h3 class="sp-title">We are so Creative</h3> -->
+					<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+					<a href="#" class="site-btn">Read More</a>
+				</div>
+				<div class="col-xl-7 offset-xl-1">
+					<figure class="intro-img mt-5 mt-xl-0">
+						<img src="img/intro.jpg" alt="">
+					</figure>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Intro section end -->
+
+	<!-- Services section start -->
+	<section class="services-section spad second-section">
+		<div class="container-fluid services-warp">
+			<div class="services-text">
+				<div class="container p-0">
+				<!-- <span class="sp-sub-title">Amazing studio</span>
+					<h3 class="sp-title">see What we offer</h3>
+					<p>Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada feugiat. Praesent malesuada congue magna at finibus. In hac habi tasse platea dictumst. Curabitur rhoncus auctor eleifend.</p>
+					<a href="#" class="site-btn">Read More</a> -->
+				</div>
+			</div>
+			<div class="container p-0">
+				<div class="row">
+					<div class="col-xl-8 offset-xl-4">
+						<!-- <div class="row">
+							<div class="col-md-6">
+								<div class="icon-box">
+									<i class="flaticon-video-camera"></i>
+									<h4>Video Footages</h4>
+									<p>Suspendisse dictum enim sit amet libero feugiat. Praesent malesuada congue magna at finibus. In hac habitasse platea dictumst. </p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="icon-box">
+									<i class="flaticon-photo-camera"></i>
+									<h4>Photo Shootings</h4>
+									<p>Congue magna at finibus. In hac habitasse platea dictumst. Curabitur rhoncus auctor eleifend. Fusce venenatis diam urna, eu pharetra arcu.</p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="icon-box">
+									<i class="flaticon-safebox"></i>
+									<h4>photo albums</h4>
+									<p>Integer nec bibendum lacus. Suspendisse dictum enim sit amet libero malesuada feugiat. Praesent malesuada congue magna at finibus. </p>
+								</div>
+							</div>
+							<div class="col-md-6">
+								<div class="icon-box">
+									<i class="flaticon-idea"></i>
+									<h4>Original ideas</h4>
+									<p>Praesent malesuada congue magna at finibus. In hac habitasse platea dictumst. Curabitur rhoncus auctor eleifend. Fusce venenatis diam urna.</p>
+								</div>
+							</div>
+						</div> -->
+					</div>
+				</div>
+			</div>
+		</div>
+		<!-- <div class="service-more-link">
+			<a href="#" class="arrow-btn">
+				<i class="fa fa-angle-right"></i>
+				<i class="fa fa-angle-right"></i>
+				<i class="fa fa-angle-right"></i>
+			</a>
+		</div> -->
+	</section>
+	<!-- Services section start end -->
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-3.2.1.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/mixitup.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
+</html>
+
+<script>
+window.onscroll = function() {myFunction()};
+
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+</script>
